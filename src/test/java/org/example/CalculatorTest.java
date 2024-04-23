ab2c65a40f68377d419f404bc3ee13a0e2baf6a5
Kayra <kayrakabakcioglu@hotmail.com>
2024-04-23 16:52:45 +0300

    Committing the project for the first time.

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,73 @@
+/*B231202350
+Kayra Kaan Kabakçıoğlu
+Software Verification and Validation - Homework 1
+*/
+
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testDivision1() {
+        assertEquals(2, Calculator.divide(10, 5));
+    }
+
+    @Test
+    void testDivision2() {
+        assertEquals(2.5, Calculator.divide(10, 4));
+    }
+
+    @Test
+    void testDivision3() {
+        assertEquals(5, Calculator.divide(12.5f, 2.5f));
+    }
+
+    @Test
+    void testDivision4() {
+        assertEquals(4, Calculator.divide(10, 2.5f));
+    }
+
+    @Test
+    void testDivision5() {
+        assertEquals(2.5f, Calculator.divide(12.5f, 5));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"10,5,2", "10,4,2.5", "12.5f, 2.5f, 5", "10, 2.5f, 4", "12.5f, 5, 2.5f"})
+    void testDivision(float a, float b, float expectedResult) {
+        assertEquals(expectedResult, Calculator.divide(a,b), 0.001f);
+    }
+
+    @Test
+    void testDivision6() {
+        Exception exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> Calculator.divide(12.5f, 0),
+                "IllegalArgumentException expected."
+        );
+
+        // Optional. To check whether the error messages match.
+        assertEquals("Illegal Argument Exception.", exception.getMessage());
+    }
+
+
+
+
+
+}
